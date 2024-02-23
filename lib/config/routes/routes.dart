966ab4c80fd09640a2a564bf5222ef102d1bac5b
habibaelmeshad <habibaelmeshad61@gmail.com>
2024-02-23 14:56:01 +0200

    app init

---
+++ lib/config/routes/routes.dart
@@ -0,0 +1,39 @@
+
+
+import 'package:flutter/material.dart';
+import 'package:graduation_project/featuers/home/presentation/pages/home.dart';
+import 'package:graduation_project/featuers/login/presentation/pages/login_screen.dart';
+import 'package:graduation_project/featuers/signup/presentation/pages/signup_screen.dart';
+
+class RoutesName{
+  static const String login="/";
+  static const String signup="signup";
+  static const String home="Home";
+
+}
+
+class AppRouter{
+  static Route onGenerate(RouteSettings settings){
+    switch(settings.name){
+      case RoutesName.login:
+        return MaterialPageRoute(builder: (context) => LoginScreen());
+      case RoutesName.signup:
+        return MaterialPageRoute(builder: (context) => SignupScreen());
+      case RoutesName.home:
+        return MaterialPageRoute(builder: (context) => HomeScreen());
+
+      default:
+        return MaterialPageRoute(builder: (context) => unDefineRoute());
+    }
+  }
+  static Widget unDefineRoute(){
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("UnDefine Route"),
+      ),
+      body: Center(
+        child: Text("Route Not found"),
+      ),
+    );
+  }
+}
