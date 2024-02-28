1e3e6a8025448afef3d828a0e3fc94ce4b419c42
habibaelmeshad <habibaelmeshad61@gmail.com>
2024-02-28 15:05:53 +0200

    app init

--- lib/featuers/login/presentation/pages/login_screen.dart
+++ lib/featuers/login/presentation/pages/login_screen.dart
@@ -1,16 +1,122 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:graduation_project/config/routes/routes.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Stack(
-        children: [
-          Image.asset("assets/images/IMG.JPG")
-        ],
+    return Container(
+      decoration: BoxDecoration(
+        image: DecorationImage(
+            image: AssetImage("assets/images/IMG.JPG"),
+        fit: BoxFit.cover)
+      ),
+      child: Scaffold(
+        resizeToAvoidBottomInset: false,
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          elevation: 0.0,
+        ),
+        backgroundColor: Colors.transparent,
+        body: Padding(
+          padding: const EdgeInsets.all(20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Text("WELCOME",
+                style:GoogleFonts.average(
+                  fontSize: 35,
+                fontWeight: FontWeight.bold),
+              ),
+              Text("To join the Teddy",
+              style: TextStyle(
+                fontSize: 18
+              ),),
+              SizedBox(height: 25,),
+              TextFormField(
+                decoration: InputDecoration(
+                  filled: true,
+                  fillColor: Colors.white,
+                  hintText: "User Name",
+                  hintStyle: TextStyle(
+                    color: Colors.grey
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20),
+                    borderSide: BorderSide(color: Colors.transparent)
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(20),
+                      borderSide: BorderSide(color: Colors.transparent)
+                  ),
+                ),
+              ),
+              SizedBox(height: 25,),
+              TextFormField(
+                decoration: InputDecoration(
+                  filled: true,
+                  fillColor: Colors.white,
+                  hintText: "Password",
+                  hintStyle: TextStyle(
+                    color: Colors.grey
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(20),
+                      borderSide: BorderSide(color: Colors.transparent)
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(20),
+                      borderSide: BorderSide(color: Colors.transparent)
+                  ),
+                ),
+              ),
+              SizedBox(height: 25,),
+              Container(
+                padding: EdgeInsets.all(10),
+                decoration: BoxDecoration(
+                  color: Colors.orange[200],
+                  borderRadius: BorderRadius.circular(15)
+                ),
+                child:Center(
+                  child:Text("Log in",
+                  style: TextStyle(
+                    fontWeight: FontWeight.w600,
+                    fontSize: 20,
+                    
+                  ),),
+                )
+              ),
+              SizedBox(height: 10,),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text("Not a member? ",
+                    style: TextStyle(
+
+                        fontWeight: FontWeight.bold
+                    ),),
+                  InkWell(
+                    onTap: (){
+                      Navigator.pushNamedAndRemoveUntil(context, RoutesName.signup, (route) => false);
+                    },
+                    child: Text("Register Now",
+                    style: TextStyle(
+                      color: Colors.blue,
+                      fontWeight: FontWeight.bold
+                    ),),
+                  )
+                ],
+              )
+
+
+
+
+            ],
+          ),
+        ),
       ),
     );
   }
--- lib/featuers/signup/presentation/pages/signup_screen.dart
+++ lib/featuers/signup/presentation/pages/signup_screen.dart
@@ -1,10 +1,140 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class SignupScreen extends StatelessWidget {
   const SignupScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Container(
+      decoration: BoxDecoration(
+          image: DecorationImage(
+              image: AssetImage("assets/images/IMG.JPG"),
+              fit: BoxFit.cover)
+      ),
+      child: Scaffold(
+        resizeToAvoidBottomInset: false,
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          elevation: 0.0,
+        ),
+        backgroundColor: Colors.transparent,
+        body: Padding(
+          padding: const EdgeInsets.all(18),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Text("WELCOME",
+                style:GoogleFonts.average(
+                    fontSize: 35,
+                    fontWeight: FontWeight.bold),
+              ),
+              Text("To join the Teddy",
+                style: TextStyle(
+                    fontSize: 18
+                ),),
+              SizedBox(height: 18,),
+              TextFormField(
+                decoration: InputDecoration(
+                  filled: true,
+                  fillColor: Colors.white,
+                  hintText: "Full Name",
+                  hintStyle: TextStyle(
+                      color: Colors.grey
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(20),
+                      borderSide: BorderSide(color: Colors.transparent)
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(20),
+                      borderSide: BorderSide(color: Colors.transparent)
+                  ),
+                ),
+              ),
+              SizedBox(height: 18,),
+              TextFormField(
+                decoration: InputDecoration(
+                  filled: true,
+                  fillColor: Colors.white,
+                  hintText: "Email",
+                  hintStyle: TextStyle(
+                      color: Colors.grey
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(20),
+                      borderSide: BorderSide(color: Colors.transparent)
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(20),
+                      borderSide: BorderSide(color: Colors.transparent)
+                  ),
+                ),
+              ),
+              SizedBox(height: 18,),
+              TextFormField(
+                decoration: InputDecoration(
+                  filled: true,
+                  fillColor: Colors.white,
+                  hintText: "Password",
+                  hintStyle: TextStyle(
+                      color: Colors.grey
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(20),
+                      borderSide: BorderSide(color: Colors.transparent)
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(20),
+                      borderSide: BorderSide(color: Colors.transparent)
+                  ),
+                ),
+              ),
+              SizedBox(height: 18,),
+              TextFormField(
+                decoration: InputDecoration(
+                  filled: true,
+                  fillColor: Colors.white,
+                  hintText: "Confirm Password",
+                  hintStyle: TextStyle(
+                      color: Colors.grey
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(20),
+                      borderSide: BorderSide(color: Colors.transparent)
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(20),
+                      borderSide: BorderSide(color: Colors.transparent)
+                  ),
+                ),
+              ),
+              SizedBox(height: 18,),
+              Container(
+                  padding: EdgeInsets.all(10),
+                  decoration: BoxDecoration(
+                      color: Colors.orange[200],
+                      borderRadius: BorderRadius.circular(15)
+                  ),
+                  child:Center(
+                    child:Text("Sign Up",
+                      style: TextStyle(
+                        fontWeight: FontWeight.w600,
+                        fontSize: 20,
+
+                      ),),
+                  )
+              ),
+
+
+
+
+
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
